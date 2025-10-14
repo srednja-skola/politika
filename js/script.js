a7f4e45b265dfda28fae525f3aa4a2069b6a3123
srednja-skola <dgavrano@gmail.com>
2025-10-14 11:33:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // footer copyleft (nova godina se mjenja automatski)
-// var copyleft = document.querySelector(".copyleft");
+var copyleft = document.querySelector(".copyleft");
 
 // rute za pojedinu stranicu
 
@@ -18,10 +18,10 @@ var route = {
     }
 }
 // aktiviraj na svim stranicama (ostaviš prazno i to znači svi linkovi) - sve one stranice na kojima želiš drukčije, postavi ispod ovog koda
-// route.add("", function() {
+route.add("", function() {
       // footer s linkom na mom imenu
-   //   copyleft.innerHTML = "Copyleft <span class='cl'>&copy;</span> " + new Date().getFullYear() + ". - autor";
-// });
+copyleft.innerHTML = "Copyleft <span class='cl'>&copy;</span> " + new Date().getFullYear() + ". - autor";
+});
 // aktiviraj samo kad je na stranici državne mature
 // route.add('drzavna-matura.html', function() {
 //   // footer s linkom na mom imenu
